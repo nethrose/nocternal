ba50195b100825d67bd1b6e38d5778e895c18fa8
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-03-31 01:23:01 -0700

    Create main.js

---
+++ src/dist/main.js
@@ -0,0 +1,13 @@
+{
+  "name": "nocternal",
+  "version": "1.0.0",
+  "description": "Nocternal Operations Center",
+  "main": "src/App.tsx",
+  "scripts": {
+    "start": "react-scripts start"
+  },
+  "dependencies": {
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2"
+  }
+}
