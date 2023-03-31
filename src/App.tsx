84ae63bacb1d0557e6363a9761d9fe85966475d0
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-03-31 00:09:58 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './components/LandingPage';
+
+function App() {
+  return (
+    <div>
+      <LandingPage title="Welcome to Nocternal" description="Your Network Operations Center" />
+      {/* Add other components or page content here */}
+    </div>
+  );
+}
+
+export default App;
