94b2b4e66211ea2e28635ee549ebdc922dde8e4c
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-03-31 02:49:00 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import LandingPage from './components/LandingPage';
 
 const App: React.FC = () => {
-  return <LandingPage title="Welcome to Nocternal" description="Your Network Operations Center!" />;
-};
+  return (
+    <div>
+      <LandingPage title="Welcome to Nocternal" description="The Nocternal Operations Center" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 export default App;
