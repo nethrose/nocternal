339af7ead36bc8eae9b5db7c975bc61a122e65e7
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-03-31 19:22:08 -0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { Pool } = require('pg');
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// PostgreSQL connection
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 const PORT = process.env.PORT || 3001;
 
