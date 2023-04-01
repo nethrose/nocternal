998e42fb1a264418f5cbe3b5a67bd8e8c14d8872
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-03-31 18:52:23 -0700

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
