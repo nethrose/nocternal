20d61a1502f26d9591f12776f69a7ebd5cef4223
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-01 17:49:35 -0700

    Update app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,9 +1,13 @@
 const request = require('supertest');
-const app = require('./app');
+const server = require('./app');
 
 describe('API tests', () => {
+  afterAll(() => {
+    server.close();
+  });
+
   test('Server should respond with status 200 on root path', async () => {
-    const response = await request(app).get('/');
+    const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
   });
 });
