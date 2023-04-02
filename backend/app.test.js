69ccc8a1198cc49132bb59327334ed1461b96f89
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-02 12:09:59 -0700

    Update app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { app, startServer } = require('./app');
+const { app, startServer, pool } = require('./app');
 const { pool } = require('./app');
 
 let server;
