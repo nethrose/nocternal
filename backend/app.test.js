5878b1dd01ca24fc8948d52c7c1d135f6d6d9039
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-02 16:35:30 -0700

    Update app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -7,16 +7,21 @@ let server;
 describe('API tests', () => {
   beforeAll(async () => {
     server = startServer();
-    await pool.connect();
+    console.log('Server started for testing');
+    const connection = await pool.connect();
+    console.log('Connected to the database for testing');
   });
 
   afterAll(async () => {
     server.close();
+    console.log('Server closed after testing');
     await pool.end(); // Close the PostgreSQL connection
+    console.log('Database connection closed after testing');
   });
 
   test('Server should respond with status 200 on root path', async () => {
     const response = await request(server).get('/');
+    console.log('Received response from root path');
     expect(response.statusCode).toBe(200);
   });
 
