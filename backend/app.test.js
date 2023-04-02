1f86a6ea1a2c48eed6378604fed3cf64493313ad
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-02 00:10:14 -0700

    Create app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const { app, startServer } = require('./app');
+const { pool } = require('./app');
 
 let server;
 
@@ -16,4 +17,9 @@ describe('API tests', () => {
     const response = await request(server).get('/');
     expect(response.statusCode).toBe(200);
   });
+  
+afterAll(() => {
+  pool.end(); // Close the PostgreSQL connection
+});
+
 });
