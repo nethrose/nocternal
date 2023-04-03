dd1697a1a0af23a3a184cfdd9f61dbed0e3df79b
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-02 23:16:51 -0700

    Create tracer.js

---
+++ backend/tracer.js
@@ -0,0 +1,41 @@
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+const { MeterProvider } = require('@opentelemetry/sdk-metrics-base');
+
+// Initialize the Tracer Provider
+const tracerProvider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'nocternal',
+  }),
+});
+
+// Configure the console exporter and span processor
+const consoleExporter = new ConsoleSpanExporter();
+const consoleSpanProcessor = new SimpleSpanProcessor(consoleExporter);
+tracerProvider.addSpanProcessor(consoleSpanProcessor);
+
+// Configure the OTLP exporter and span processor
+const otlpExporter = new CollectorTraceExporter();
+const otlpSpanProcessor = new SimpleSpanProcessor(otlpExporter);
+tracerProvider.addSpanProcessor(otlpSpanProcessor);
+
+// Register the tracer provider
+tracerProvider.register();
+
+// Initialize the Meter Provider
+const meterProvider = new MeterProvider();
+
+// Register the instrumentations
+registerInstrumentations({
+  tracerProvider,
+  meterProvider,
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+  ],
+});
+
+console.log('Tracer and meter providers initialized.');
