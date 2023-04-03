55b7f382a0db7b4c9f297ed72693888fb40c19ec
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-02 20:14:33 -0700

    Update app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -1,31 +1,24 @@
 const request = require('supertest');
-const { app, startServer, pool } = require('./app');
+const { app, startServer, pool } = require('../app');
 
 let server;
-beforeAll(() => {
-  console.log('Server started for testing');
-  server = startServer({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+
+beforeAll(async () => {
+  server = await startServer();
 });
 
 afterAll(async () => {
-  server.close();
-  console.log('Server closed after testing');
-  await pool.end(); // Close the PostgreSQL connection
-}, 20000);
+  await server.close();
+  await pool.end();
+});
 
-describe('API tests', () => {
-  test('Server should respond with status 200 on root path', async () => {
-    const response = await request(server).get('/');
-    console.log('Received response from root path');
-    expect(response.statusCode).toBe(200);
-  });
+test('GET / should return 200', async () => {
+  const res = await request(app).get('/');
+  expect(res.statusCode).toEqual(200);
+  expect(res.text).toEqual('Hello, world!');
+});
+
+test('POST /ingest should return 200', async () => {
+  const res = await request(app).post('/ingest').send({ traceId: 'sampleTraceId' });
+  expect(res.statusCode).toEqual(200);
 });
