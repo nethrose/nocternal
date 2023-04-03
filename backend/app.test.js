4570c7d125c3d2746737c68032ebf2332192b242
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-02 17:17:43 -0700

    Update app.test.js

--- backend/app.test.js
+++ backend/app.test.js
@@ -2,26 +2,30 @@ const request = require('supertest');
 const { app, startServer, pool } = require('./app');
 
 let server;
+beforeAll(() => {
+  console.log('Server started for testing');
+  server = startServer({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+});
+
+afterAll(async () => {
+  server.close();
+  console.log('Server closed after testing');
+  await pool.end(); // Close the PostgreSQL connection
+}, 20000);
 
 describe('API tests', () => {
-  beforeAll(async () => {
-    console.log('Server started for testing');
-    server = startServer();
-    const client = await pool.connect();
-    console.log('Connected to the database for testing');
-    client.release();
-  });
-
-  afterAll(async () => {
-    server.close();
-    console.log('Server closed after testing');
-    await pool.end(); // Close the PostgreSQL connection
-  }, 20000);
-
   test('Server should respond with status 200 on root path', async () => {
     const response = await request(server).get('/');
     console.log('Received response from root path');
     expect(response.statusCode).toBe(200);
   });
-
 });
