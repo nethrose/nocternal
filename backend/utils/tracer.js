941347efa86e4f003b51721d3b70cc336851d37d
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-04 13:36:00 -0700

    Update tracer.js

--- backend/utils/tracer.js
+++ backend/utils/tracer.js
@@ -4,17 +4,26 @@ const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
+// Define your service name
+const serviceName = 'nocternal-backend'; // Change this to 'nocternal-main' for the main service
+
 // Tracer setup
 const traceProvider = new BasicTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'your-service-name',
+    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
   }),
 });
-const traceExporter = new OTLPTraceExporter(traceCollectorOptions);
-traceProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
+
+// Configure the OTLPTraceExporter with the appropriate endpoint
+const traceCollectorOptions = {
+  endpoint: 'https://your-opentelemetry-collector-endpoint:port', // Replace with your OpenTelemetry Collector endpoint and port
+};
+
+const otlpTraceExporter = new OTLPTraceExporter(traceCollectorOptions);
+traceProvider.addSpanProcessor(new BatchSpanProcessor(otlpTraceExporter));
 traceProvider.register();
 
 // Custom tracer
-const tracer = traceProvider.getTracer('your-service-name');
+const tracer = traceProvider.getTracer(serviceName);
 
 module.exports = { tracer };
